eff0e4a8eafa2fedb7b55717c253eae599b847bf
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2024-12-02 17:01:37 +0100

    Drone example

---
+++ drone/sounds.rs
@@ -0,0 +1,4 @@
+use std::string::ToString;
+
+pub const SOUND1: String = "https://github.com/Rust-In-Peace-AP/sound-effects/blob/main/categories/success/quackable/.quackSucc1".to_string();
+pub const SOUND2: String = "".to_string();
