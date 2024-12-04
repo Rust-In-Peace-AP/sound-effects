1c30ea4ee7919104feca29eddccddd50f4fe8d94
EnricoBolognesi <enrico.bolognesi@studenti.unitn.it>
2024-12-04 10:12:57 +0100

    modifica handle_command

--- drone/drone_usage.rs
+++ drone/drone_usage.rs
@@ -72,6 +72,8 @@ impl MyDrone {
     }
     fn handle_command(&mut self, command: DroneCommand) {
         match command {
+            //TO DO: aggiungere la condivisione felle modifiche tramite flood response al resto della rete!!!!!!!
+            //per tutti i metodi
             DroneCommand::AddSender(node_id, sender) => {
                 self.packet_send.insert(node_id, sender);
             },
