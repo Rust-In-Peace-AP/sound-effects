9b40b9624ad0d2d1521a1cae47abefe261bba8f6
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2024-12-04 09:34:27 +0100

    Removed drone

--- drone/sounds.rs
+++
@@ -1,4 +0,0 @@
-use std::string::ToString;
-
-pub const SOUND1: String = "https://github.com/Rust-In-Peace-AP/sound-effects/blob/main/categories/success/quackable/.quackSucc1".to_string();
-pub const SOUND2: String = "".to_string();
