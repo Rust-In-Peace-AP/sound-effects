6df74b6f45aca38979e2a483931caae4ca80631e
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2024-12-08 16:59:04 +0100

    Windows drone added

---
+++ drones_version/windows_drone/target/debug/deps/bitflags-e3f1d0f939370cde.d
@@ -0,0 +1,7 @@
+C:\Users\Admin\Desktop\RustInPeace\sound-effects\drones_version\target\debug\deps\libbitflags-e3f1d0f939370cde.rmeta: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bitflags-1.3.2\src\lib.rs
+
+C:\Users\Admin\Desktop\RustInPeace\sound-effects\drones_version\target\debug\deps\libbitflags-e3f1d0f939370cde.rlib: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bitflags-1.3.2\src\lib.rs
+
+C:\Users\Admin\Desktop\RustInPeace\sound-effects\drones_version\target\debug\deps\bitflags-e3f1d0f939370cde.d: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bitflags-1.3.2\src\lib.rs
+
+C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bitflags-1.3.2\src\lib.rs:
